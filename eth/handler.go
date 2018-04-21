9e556dc39eb8d902cc06685f1ac609c8c8395e19
Ryan Schneider <ryanleeschneider@gmail.com>
2018-04-20 17:08:03 -0700

    eth: hacky quick fix to larger txChanSize to try to prevent buffer backup in `Feed.Send(...)`.

--- eth/handler.go
+++ eth/handler.go
@@ -48,7 +48,7 @@ const (
 
 	// txChanSize is the size of channel listening to TxPreEvent.
 	// The number is referenced from the size of tx pool.
-	txChanSize = 4096
+	txChanSize = 65536 // HACKED to bigger
 )
 
 var (
