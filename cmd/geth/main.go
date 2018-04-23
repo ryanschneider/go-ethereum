f3d1f783296e0f41e344bb3dfdebd5e715e83d47
Ryan Schneider <ryanleeschneider@gmail.com>
2018-04-23 16:36:30 -0700

    geth: add a `--txpool.noremotes` option to skip adding remote transactions to the pool.

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -78,6 +78,7 @@ var (
 		utils.EthashDatasetsOnDiskFlag,
 		utils.TxPoolLocalsFlag,
 		utils.TxPoolNoLocalsFlag,
+		utils.TxPoolNoRemotesFlag,
 		utils.TxPoolJournalFlag,
 		utils.TxPoolRejournalFlag,
 		utils.TxPoolPriceLimitFlag,
