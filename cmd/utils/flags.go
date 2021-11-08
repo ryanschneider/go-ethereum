32bccff2504b4ed6c735b1ed883b3c2ae0dacac3
Ryan Schneider <ryanleeschneider@gmail.com>
2021-11-08 12:22:48 -0800

    eth,les: default to 10x the block limit if RPC gas cap is not set

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -1584,7 +1584,7 @@ func SetEthConfig(ctx *cli.Context, stack *node.Node, cfg *ethconfig.Config) {
 	if cfg.RPCGasCap != 0 {
 		log.Info("Set global gas cap", "cap", cfg.RPCGasCap)
 	} else {
-		log.Info("Global gas cap disabled")
+		log.Info("Global gas cap set to 10x block gas limit")
 	}
 	if ctx.GlobalIsSet(RPCGlobalEVMTimeoutFlag.Name) {
 		cfg.RPCEVMTimeout = ctx.GlobalDuration(RPCGlobalEVMTimeoutFlag.Name)
