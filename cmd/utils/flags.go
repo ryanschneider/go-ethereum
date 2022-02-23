5e493685bb8d3ecb61730452f04a6fb6f7ecb33a
Ryan Schneider <ryanleeschneider@gmail.com>
2022-02-23 09:06:00 -0800

    cmd/utils, cmd/geth: Default to listening on all HTTP and WebSocket paths

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -576,7 +576,7 @@ var (
 	HTTPPathPrefixFlag = cli.StringFlag{
 		Name:  "http.rpcprefix",
 		Usage: "HTTP path path prefix on which JSON-RPC is served. Use '/' to serve on all paths.",
-		Value: "",
+		Value: "/",
 	}
 	GraphQLEnabledFlag = cli.BoolFlag{
 		Name:  "graphql",
@@ -619,7 +619,7 @@ var (
 	WSPathPrefixFlag = cli.StringFlag{
 		Name:  "ws.rpcprefix",
 		Usage: "HTTP path prefix on which JSON-RPC is served. Use '/' to serve on all paths.",
-		Value: "",
+		Value: "/",
 	}
 	ExecFlag = cli.StringFlag{
 		Name:  "exec",
