1309acb22b153595a07b1155825eae3ba3656cc5
Ryan Schneider <ryanleeschneider@gmail.com>
2024-01-24 08:45:18 -0800

    eth/ethconfig: Disable tx fee cap by default

--- eth/ethconfig/config.go
+++ eth/ethconfig/config.go
@@ -66,7 +66,7 @@ var Defaults = Config{
 	RPCGasCap:          0,
 	RPCEVMTimeout:      5 * time.Second,
 	GPO:                FullNodeGPO,
-	RPCTxFeeCap:        1, // 1 ether
+	RPCTxFeeCap:        0, // no fee cap
 }
 
 //go:generate go run github.com/fjl/gencodec -type Config -formats toml -out gen_config.go
